6b48ed3bc627affc9146c13279b0cccff8def500
rewwea <avandronov10@gmail.com>
2025-01-22 15:01:39 +0500

    Add IComparable and ICloneable interfaces to Animal class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 interface IMovable
 {
     void Move(int x, int y);
@@ -13,7 +14,8 @@ interface ISoundable
 {
     void MakeSound();
 }
-class Animal : IMovable, IRenamable, ISoundable
+
+class Animal : IMovable, IRenamable, ISoundable, IComparable, ICloneable
 {
     public string Name { get; private set; }
     public int X { get; private set; }
@@ -43,6 +45,26 @@ class Animal : IMovable, IRenamable, ISoundable
     {
         Console.WriteLine($"{Name} издает звук: Мяу!");
     }
+
+    public int CompareTo(object obj)
+    {
+        if (obj == null) return 1;
+
+        Animal other = obj as Animal;
+        if (other == null) throw new ArgumentException("Object is not an Animal");
+
+        return string.Compare(this.Name, other.Name, StringComparison.Ordinal);
+    }
+
+    public object Clone()
+    {
+        return new Animal(this.Name, this.X, this.Y);
+    }
+
+    public override string ToString()
+    {
+        return $"Animal: {Name}, Location: ({X}, {Y})";
+    }
 }
 
 class Program
@@ -52,9 +74,13 @@ class Program
         Animal cat = new Animal("Барсик", 0, 0);
 
         cat.Move(5, 10);
-
         cat.Rename("Мурзик");
-        
         cat.MakeSound();
+
+        Animal dog = new Animal("Шарик", 10, 20);
+        Console.WriteLine(cat.CompareTo(dog));
+
+        Animal clonedCat = (Animal)cat.Clone();
+        Console.WriteLine($"Клон: {clonedCat}");
     }
 }
